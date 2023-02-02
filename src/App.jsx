fc38a26e95d3ee78fe12d7ee9a9f46a1131f7505
aziz-codes <azizisoffline@gmail.com>
2023-02-02 23:50:29 +0500

    protected routes implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navigation/Navbar";
 import { Home, About, Contact, Users, Page404, Login } from "./pages/index";
-// import ProtectedRoutes from "./components/Navigation/ProtectedRoutes";
+import ProtectedRoutes from "./components/Navigation/ProtectedRoutes";
 const App = () => {
   let user = false;
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" index component={Home} user={user} />
-        <Route path="/about" component={About} user={user} />
-        <Route path="/contact" component={Contact} user={user} />
-        <Route path="/users" component={Users} user={user} />
-        <Route path="*" component={Page404} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      {user ? (
+        <>
+          {" "}
+          <Navbar />
+          <Routes>
+            <Route element={<ProtectedRoutes auth={user} />}>
+              <Route path="/" exact component={Home} user={user} />
+              <Route path="/about" component={About} user={user} />
+              <Route path="/contact" component={Contact} user={user} />
+              <Route path="/users" component={Users} user={user} />
+              <Route path="*" component={Page404} />
+            </Route>
+
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </>
+      ) : (
+        <Login />
+      )}
     </BrowserRouter>
   );
 };
--- src/components/Navigation/ProtectedRoutes.jsx
+++ src/components/Navigation/ProtectedRoutes.jsx
@@ -1,22 +1,7 @@
-import { React } from "react";
-import { Route, Navigate } from "react-router-dom";
-const ProtectedRoutes = ({ user, component: Component, ...rest }) => {
-  return (
-    <>
-      <Route
-        {...rest}
-        render={(props) => {
-          if (user) {
-            return <Component />;
-          } else
-            return (
-              <Navigate
-                to={{ pathname: "/login", state: { from: props.location } }}
-              />
-            );
-        }}
-      />
-    </>
-  );
+import { Outlet, Navigate } from "react-router-dom";
+
+const ProtectedRoutes = ({ auth }) => {
+  return auth ? <Outlet /> : <Navigate to="/login" />;
 };
+
 export default ProtectedRoutes;
