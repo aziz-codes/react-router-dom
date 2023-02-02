3966911e2edf28591b574a56d509766ac23e7ae2
aziz-codes <azizisoffline@gmail.com>
2023-02-02 23:21:58 +0500

    typicall routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navigation/Navbar";
 import { Home, About, Contact, Users, Page404, Login } from "./pages/index";
-import ProtectedRoutes from "./components/Navigation/ProtectedRoutes";
+// import ProtectedRoutes from "./components/Navigation/ProtectedRoutes";
 const App = () => {
   let user = false;
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <ProtectedRoutes path="/" index component={Home} user={user} />
-        <ProtectedRoutes path="/about" component={About} user={user} />
-        <ProtectedRoutes path="/contact" component={Contact} user={user} />
-        <ProtectedRoutes path="/users" component={Users} user={user} />
-        <ProtectedRoutes path="*" component={Page404} />
+        <Route path="/" index component={Home} user={user} />
+        <Route path="/about" component={About} user={user} />
+        <Route path="/contact" component={Contact} user={user} />
+        <Route path="/users" component={Users} user={user} />
+        <Route path="*" component={Page404} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
